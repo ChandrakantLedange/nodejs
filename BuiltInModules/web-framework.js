22bcaae5c40701eb82101c8f99b3297809240d85
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2025-03-08 23:37:19 +0530

    NodeJs: What is web framework and libuv.

---
+++ BuiltInModules/web-framework.js
@@ -0,0 +1,10 @@
+//What is web framework
+// -A framework simply abstracts the lower level code allowing you to focus the requirement than th code itself.
+
+//For Ex - Angular, React ,Vue are all framework/library that help you build user interface without having to rely on the lower level DOM API in javascript.
+
+//There are frameworks to build web or mobile applications without havig to rely on the HTTP module in nodejs
+
+//For Ex - Express js ,nest, hapi, koa and sails.
+
+//They build on top of the HTTP module making it easier for you to implememntall the feature.
---
+++ libuv/libuv.js
@@ -0,0 +1,9 @@
+//what is libuv ?
+//libuv is a cross platform open source library written in c language.
+
+//Why libuv ?
+//handles asynchronous no-blocking operations in Node.js
+
+//How ?
+//Thread pool
+//Event loop

15a186d867b28b44b250d6b8d1d218b84783d8ce
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2025-03-08 22:34:00 +0530

    NodeJs : JSON Response

--- BuiltInModules/http_module.js
+++ BuiltInModules/http_module.js
@@ -1,9 +1,14 @@
 const http  = require("node:http");
 
 const server = http.createServer((req, res)=>{
-    console.log("req:",req)
-    res.writeHead(200,{"content-type":"text/plain"});
-    res.end("Welcome to the server")
+    // console.log("req:",req)
+
+    const superHero ={
+        firstName : 'superman',
+        lastName : 'batman'
+    }
+    res.writeHead(200,{"content-type":"application/json"});
+    res.end(JSON.stringify(superHero));
 
 });
 

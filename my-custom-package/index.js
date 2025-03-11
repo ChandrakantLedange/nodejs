2779267145a7c211f8a8d74d82396aa87c314d3d
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2025-03-11 09:44:08 +0530

    NodeJs: npm and custom package.json creation

---
+++ my-custom-package/index.js
@@ -0,0 +1,12 @@
+function greet(name){
+    return console.log(`Hello ${name}, Welcome to custom package json`)
+}
+
+
+module.exports = greet;
+
+
+//create package.json file
+//>npm init
+
+//> npm init --yes - create file direct with default setup
